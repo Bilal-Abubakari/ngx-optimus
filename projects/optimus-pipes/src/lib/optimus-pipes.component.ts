4bc3ce48159a8dd0c9da99ff64d67d84dba058fb
= <abubakaribilal99@gmail.com>
2025-03-30 00:43:30 +0000

    chore: initial optimus pipes library setup

---
+++ projects/optimus-pipes/src/lib/optimus-pipes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-optimus-pipes',
+  imports: [],
+  template: `
+    <p>
+      optimus-pipes works!
+    </p>
+  `,
+  styles: ``
+})
+export class OptimusPipesComponent {
+
+}
