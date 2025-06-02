92feca942f71d2d0fe541d3a9d5b9172c97a95bd
= <abubakaribilal99@gmail.com>
2025-06-02 11:32:20 +0000

    feat: implement interactive demo for optimus pipes with user input and output display

--- projects/optimus-pipe-demo/src/app/app.component.ts
+++ projects/optimus-pipe-demo/src/app/app.component.ts
@@ -1,12 +1,45 @@
 import { Component } from "@angular/core";
-import { SentenceCasePipe } from "optimus-pipes";
+import { FormsModule } from "@angular/forms";
+import { SentenceCasePipe } from "../../../optimus-pipes/src/lib/pipes/sentence-case.pipe";
+
+interface PipeInfo {
+  name: string;
+  displayName: string;
+  description: string;
+  initialInput: string;
+}
 
 @Component({
   selector: "app-root",
-  imports: [SentenceCasePipe],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  styleUrls: ["./app.component.scss"],
+  imports: [FormsModule, SentenceCasePipe],
 })
 export class AppComponent {
-  title = "optimus-pipe-demo";
+  public userInput: string = "hello_world_example";
+
+  public availablePipes: PipeInfo[] = [
+    {
+      name: "sentenceCase",
+      displayName: "Sentence Case",
+      description:
+        "Converts a string into sentence case. Handles camelCase, PascalCase, snake_case, and kebab-case inputs.",
+      initialInput: "hello_World-Example123",
+    },
+  ];
+  public selectedPipeName = this.availablePipes[0].name;
+  public currentSelectPipe = this.availablePipes[0];
+
+  get currentInitialInput(): string {
+    const selectedPipe = this.availablePipes.find(
+      p => p.name === this.selectedPipeName
+    );
+    return selectedPipe ? selectedPipe.initialInput : "testInput";
+  }
+
+  public selectedPipeExists() {
+    return this.availablePipes.find(
+      ({ name }) => name === this.selectedPipeName
+    );
+  }
 }
